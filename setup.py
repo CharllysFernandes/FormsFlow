c5f2cee6b712d95b5e81312f4e8b22c11f399f30
Charllys Fernandes <charllys.daiany@gmail.com>
2023-10-12 21:46:46 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='meu_projeto',
+    version='1.0',
+    packages=['meu_modulo'],
+    install_requires=[
+        'selenium',
+        'pandas',
+    ],
+)
